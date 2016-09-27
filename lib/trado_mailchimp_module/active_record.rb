b25d2854cb62b2612808799b5532d38503b2ac7c
Tom Dallimore <me@tomdallimore.com>
2016-09-27 19:51:40 +0700

    Minor bugfix for newsletter validation and modified callback to trigger on update too.

--- lib/trado_mailchimp_module/active_record.rb
+++ lib/trado_mailchimp_module/active_record.rb
@@ -7,14 +7,18 @@ module TradoMailchimpModule
                 attr_accessible :newsletter
                 attr_accessor :newsletter
 
-                after_commit :send_email_to_mailchimp,              on: :create, if: :newsletter?
+                after_commit :send_email_to_mailchimp,              on: [:create, :update], if: :newsletter?
 
                 define_method("send_email_to_mailchimp") do
-                    Gibbon::Request.lists(Store.settings.mailchimp_list_id).members.create(body: {email_address: self.email, status: "subscribed", merge_fields: {FNAME: self.billing_address.first_name, LNAME: self.billing_address.last_name}}) if Store.settings.mailchimp_list_id.present? && self.email.present? && self.billing_address.present?
+                    begin 
+                        Gibbon::Request.lists(Store.settings.mailchimp_list_id).members.create(body: {email_address: self.email, status: "subscribed", merge_fields: {FNAME: self.billing_address.first_name, LNAME: self.billing_address.last_name}}) if Store.settings.mailchimp_list_id.present? && self.email.present? && self.billing_address.present?
+                    rescue Gibbon::MailChimpError
+                        return true
+                    end
                 end
 
                 define_method("newsletter?") do
-                    newsletter == 1 ? true : false
+                    newsletter == "1" ? true : false
                 end
             end
 
