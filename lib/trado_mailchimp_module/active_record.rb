6172723fbaf98e7fa5e3a75a27501f1c3e8125da
Tom Dallimore <me@tomdallimore.com>
2016-09-27 19:28:42 +0700

    Fixed bug in newsletter checkbox validation.

--- lib/trado_mailchimp_module/active_record.rb
+++ lib/trado_mailchimp_module/active_record.rb
@@ -7,10 +7,14 @@ module TradoMailchimpModule
                 attr_accessible :newsletter
                 attr_accessor :newsletter
 
-                after_commit :send_email_to_mailchimp,              on: :create, if: :newsletter
+                after_commit :send_email_to_mailchimp,              on: :create, if: :newsletter?
 
                 define_method("send_email_to_mailchimp") do
                     Gibbon::Request.lists(Store.settings.mailchimp_list_id).members.create(body: {email_address: self.email, status: "subscribed", merge_fields: {FNAME: self.billing_address.first_name, LNAME: self.billing_address.last_name}}) if Store.settings.mailchimp_list_id.present? && self.email.present? && self.billing_address.present?
+                end
+
+                define_method("newsletter?") do
+                    newsletter == 1 ? true : false
                 end
             end
 
